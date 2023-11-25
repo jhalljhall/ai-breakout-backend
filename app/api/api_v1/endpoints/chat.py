7ab7d0b90804ac6e9ca15146255cde0c89476983
Justin Hall <hall.justin@justinhall.com>
2023-11-25 07:06:44 +0000

    creating chat endpoints

--- app/api/api_v1/endpoints/chat.py
+++ app/api/api_v1/endpoints/chat.py
@@ -13,11 +13,8 @@ from app.core import security
 router = APIRouter()
 
 @router.get("/send", response_model=List[schemas.StorySessionPrompt])
-def read_users(
-    db: Session = Depends(deps.get_db),
-    skip: int = 0,
-    limit: int = 100,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
+def send_prompt(
+    db: Session = Depends(deps.get_db)
 ) -> Any:
     """
     Retrieve users.
